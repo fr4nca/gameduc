120d1a1e58c98eb8470ff1b5c5ba772e8355155e
Victor Almeida França <fr4ncacc@gmail.com>
2019-04-06 12:06:38 -0300

    implementing progress bar

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -1,6 +1,20 @@
 import React from "react";
 
+import moment from "moment";
+
+const getDiffDays = (date1, date2) => {
+  let dia1 = new Date(date1).getTime() / 1000;
+  let dia2 = new Date(date2).getTime() / 1000;
+  let today = new Date().getTime() / 1000;
+
+  let perc = (today * 100) / dia2;
+  console.log(today, dia1, dia2, perc);
+
+  return perc;
+};
+
 const Game = ({ game }) => {
+  const times = getDiffDays(game.dta_inicio, game.dta_fim);
   return (
     <div className="card">
       <header className="card-header">
@@ -14,11 +28,11 @@ const Game = ({ game }) => {
           <p>
             <strong>Data de finalização:</strong> {game.dta_fim}
           </p>
-          <p>
-            <strong>Disciplina: </strong>
-          </p>
         </div>
       </div>
+      <progress class="progress is-link" value="30" max="100">
+        {times.perc}
+      </progress>
       <footer className="card-footer">
         <a href="#!" className="card-footer-item">
           Visitar
