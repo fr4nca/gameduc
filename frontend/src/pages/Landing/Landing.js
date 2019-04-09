6359016ceb38fcf55c4cc217c2900cec976ed6d6
Lucas Freitas <luquex_skynet@hotmail.com>
2019-04-08 22:29:35 -0300

    Adicionando imagem e apanhando pro css

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,20 +1,28 @@
 import React from "react";
-
+import Primeiro from "../../image/primeiro.png";
 
 
 const Landing = () => {
   return (
     <div>
 
-      
-          <div class="box has-background-light">
-            <h1 class="is-size-3">Gameduc</h1>
-            <p class="is-size-6">O que é?</p>
-            <p class="is-size-6">Gameduc é uma plataforma em que você compete com outros alunos que estão cadastrados no mesmo game que você!</p>
-            <p>teste</p>
 
+      <div class="box has-background-light">
+        <div class="columns">
+          <h1 class="is-size-3 column">Gameduc</h1>
+        </div>
+        <div class="columns">
+          <p class="is-size-6 column">O que é?
+            <br />
+            <br />
+            <p>Gameduc é uma plataforma para professores e alunos, em que alunos irão competir entre si para ser o melhor!</p>
+          </p>
+
+          <div class="column">
+            <figure class='image is-5x3'><img src={Primeiro}></img></figure>
+          </div>
+        </div>
       </div>
-
 
     </div>
   )
