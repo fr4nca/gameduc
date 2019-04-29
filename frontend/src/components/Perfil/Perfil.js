8b97fde211fb99973940b49fc97ecc7fc824b7ac
Thaynara <thaycic@hotmail.com>
2019-04-29 15:57:52 +0000

    frontend - Perfil

--- frontend/src/components/Perfil/Perfil.js
+++ frontend/src/components/Perfil/Perfil.js
@@ -3,9 +3,45 @@ import React, { Component } from "react";
 export class Perfil extends Component {
   render() {
     return (
-      <div>
-        <h1>Perfil</h1>
-      </div>
+      <>
+      <div className="columns">
+        <div className="column"> 
+        <h1 className="title is-1">
+          <span>
+            <i className="fas fa-id-badge has-text-link" /> / <i className="fas fa-id-card has-text-link" />  Perfil
+          </span>
+        </h1>
+        </div>
+        <div className="column"> 
+          <span>
+            <button
+              className="button is-rounded is-link is-pulled-right"
+            >
+              Editar Perfil
+            </button>
+          </span>
+        </div>
+        </div>
+        <div className="box">
+          <h3 className="subtitle is-3">Seus dados</h3>
+          <hr />
+          <strong>Nome: </strong>Thaynara Ronan Victor
+          <hr />
+          <strong>Data de nascimento: </strong>14/02/2016  
+          <hr />
+          <strong>Graduação: </strong>Ciência da Computação
+          <hr />
+          <strong>Email: </strong>newthayprofessor@professor.com 
+        </div>
+        
+        <span>
+          <button
+            className="button is-rounded is-link is-pulled-right"
+          >
+            Editar Perfil
+          </button>
+        </span>
+      </>
     );
   }
 }
