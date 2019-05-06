cece1feb30c831af09afb03811ccc73c0d734a39
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-06 20:30:30 -0300

    new mobile

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,42 +1,47 @@
-import React from 'react';
+import React from "react";
 
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  StatusBar
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    alignItems: "center",
     flex: 1,
-    paddingHorizontal: 20,
+    paddingHorizontal: 20
   },
   fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
+    fontWeight: "bold",
+    marginTop: 5
   },
   instructions: {
-    color: '#DDD',
+    color: "#DDD",
     fontSize: 14,
     marginTop: 20,
-    textAlign: 'center',
+    textAlign: "center"
   },
   logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
+    height: Dimensions.get("window").height * 0.11,
+    marginVertical: Dimensions.get("window").height * 0.11,
+    width: Dimensions.get("window").height * 0.11 * (1950 / 662)
   },
   welcome: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
 
 const Main = () => (
   <ImageBackground
     source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
+      uri: "https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png"
     }}
     style={styles.container}
     resizeMode="cover"
@@ -44,15 +49,20 @@ const Main = () => (
     <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
     <Image
       source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
+        uri:
+          "https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png"
       }}
       style={styles.logo}
       resizeMode="contain"
     />
     <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
+    <Text style={styles.instructions}>
+      Essa é a tela principal da sua aplicação =)
+    </Text>
     <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
+    <Text style={[styles.instructions, styles.fileName]}>
+      src/pages/Main/index.js
+    </Text>
   </ImageBackground>
 );
 
