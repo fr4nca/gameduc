4acb3dd8d4d4a4e8e9258774321fba04b0e5a684
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-07 12:45:48 -0300

    finalizing docker

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -3,7 +3,7 @@ let config = {};
 if (process.env.NODE_ENV !== "production") {
   config = { apiUrl: "http://localhost:5000/api" };
 } else {
-  config = { apiUrl: "http://178.128.82.157/api" };
+  config = { apiUrl: process.env.REACT_APP_API_URL };
 }
 
 export default config;
