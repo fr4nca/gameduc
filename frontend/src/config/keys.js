49035a0ffb253541cccf8445440ebfc21c32b159
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-07 11:03:03 -0300

    docker

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -1,9 +1,9 @@
 let config = {};
 
 if (process.env.NODE_ENV !== "production") {
-  config = { apiUrl: "http://localhost:5000/" };
+  config = { apiUrl: "http://localhost:5000/api" };
 } else {
-  config = { apiUrl: "http://142.93.52.18/api" };
+  config = { apiUrl: "http://178.128.82.157/api" };
 }
 
 export default config;
