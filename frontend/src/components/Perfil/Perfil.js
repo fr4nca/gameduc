f66c6f05ab358fe9caf9fb1462f1b051daced869
Thaynara <thaycic@hotmail.com>
2019-05-08 10:10:02 +0000

    GetProfile

--- frontend/src/components/Perfil/Perfil.js
+++ frontend/src/components/Perfil/Perfil.js
@@ -1,49 +1,57 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+import moment from "moment";
 export class Perfil extends Component {
   render() {
+    const { profile, user} = this.props.auth;
+
     return (
       <>
-      <div className="columns">
-        <div className="column"> 
-        <h1 className="title is-1">
-          <span>
-            <i className="fas fa-id-badge has-text-link" /> / <i className="fas fa-id-card has-text-link" />  Perfil
-          </span>
-        </h1>
-        </div>
-        <div className="column"> 
-          <span>
-            <button
-              className="button is-rounded is-link is-pulled-right"
-            >
-              Editar Perfil
-            </button>
-          </span>
-        </div>
+        <div className="columns">
+          <div className="column">
+            <h1 className="title is-1">
+              <span>
+                <i className="fas fa-id-card has-text-link" /> Perfil
+              </span>
+            </h1>
+          </div>
+          <div className="column">
+            <span>
+              <button className="button is-rounded is-link is-pulled-right">
+                Editar Perfil
+              </button>
+            </span>
+          </div>
         </div>
         <div className="box">
           <h3 className="subtitle is-3">Seus dados</h3>
           <hr />
-          <strong>Nome: </strong>Thaynara Ronan Victor
+          <strong>Matricula: </strong>{profile.matricula}
           <hr />
-          <strong>Data de nascimento: </strong>14/02/2016  
+          <strong>Nome: </strong>
+          {profile.nome} {profile.sobrenome}
           <hr />
-          <strong>Graduação: </strong>Ciência da Computação
+          <strong>Data de nascimento: </strong>
+          {moment(profile.dta_nascimento).format("DD/MM/YYYY")}
           <hr />
-          <strong>Email: </strong>newthayprofessor@professor.com 
+          { user.papel === 'professor' ? (<div>
+            <strong>Graduação: </strong>{profile.graduacao}
+            </div>
+          ):(<div>
+            <strong>Curso: </strong>{profile.curso}
+            </div>)}
+          <hr />
+          <strong>Email: </strong>newthayprofessor@professor.com
         </div>
-        
-        <span>
-          <button
-            className="button is-rounded is-link is-pulled-right"
-          >
-            Editar Perfil
-          </button>
-        </span>
       </>
     );
   }
 }
 
-export default Perfil;
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Perfil);
