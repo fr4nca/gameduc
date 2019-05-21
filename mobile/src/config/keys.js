a976c8759959e3ba95f8e0e4a16bdc9454a2a0ec
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-21 15:28:02 -0300

    apapap

--- mobile/src/config/keys.js
+++ mobile/src/config/keys.js
@@ -3,7 +3,7 @@ let config = {};
 if (__DEV__) {
   config = { apiUrl: "http://10.29.19.120:5000/api" };
 } else {
-  config = { apiUrl: "http://www.gameeduc.com.br/api" };
+  config = { apiUrl: "https://gameduc-api.herokuapp.com/" };
 }
 
 export default config;
