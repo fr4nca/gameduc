5f0e8169ce98df5ce6cd39c517c9acc8d0ad42c8
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-27 10:10:53 -0300

    token never expires

--- mobile/src/config/keys.js
+++ mobile/src/config/keys.js
@@ -1,6 +1,6 @@
 let config = {};
 
-if (__DEV__) config = { apiUrl: "https://192.168.0.12:5000/api" };
+if (__DEV__) config = { apiUrl: "https://gameduc-api.herokuapp.com/api" };
 else config = { apiUrl: "https://gameduc-api.herokuapp.com/api" };
 
 export default config;
